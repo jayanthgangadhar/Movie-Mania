9a6b30f987f0c8f427461f9b6d1f88b0f4163c21
jayanthgangadhar <gangadhar.j@husky.neu.edu>
2017-06-21 00:07:39 -0400

    bug

--- assignment_server/services/widget.service.server.js
+++ assignment_server/services/widget.service.server.js
@@ -162,7 +162,7 @@ function createWidget(req,res) {
                 widget.url = '/assignment/uploads/'+filename;
                 widgetModel.updateWidget(widgetId,widget)
                     .then(function () {
-                        var callbackUrl= "website/"+websiteId+"/page/"+pageId+"/widget";
+                        var callbackUrl= "/assignment/#!/website/"+websiteId+"/page/"+pageId+"/widget";
                         res.redirect(callbackUrl);
 
                     });
