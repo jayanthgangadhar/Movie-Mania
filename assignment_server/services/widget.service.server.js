0c7a07c6904b7d0afd6a7eae972fc7a680aaee14
jayanthgangadhar <gangadhar.j@husky.neu.edu>
2017-06-20 23:48:05 -0400

    fix

--- assignment_server/services/widget.service.server.js
+++ assignment_server/services/widget.service.server.js
@@ -163,7 +163,7 @@ function createWidget(req,res) {
                 widgetModel.updateWidget(widgetId,widget)
                     .then(function () {
 
-                        var callbackUrl= "website/"+websiteId+"/page/"+pageId+"/widget";
+                        var callbackUrl= "/page/"+pageId+"/widget";
                         res.redirect(callbackUrl);
 
                     });
